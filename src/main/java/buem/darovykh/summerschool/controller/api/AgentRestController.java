51befe2d3350537b5cd9bee2c36640b16d458823
Yevhen Darovykh <darovykhy@gmail.com>
2022-08-26 07:51:59 +0200

    rest added

---
+++ src/main/java/buem/darovykh/summerschool/controller/api/AgentRestController.java
@@ -0,0 +1,40 @@
+package buem.darovykh.summerschool.controller.api;
+
+import buem.darovykh.summerschool.model.Agent;
+import buem.darovykh.summerschool.service.agent.interfaces.IAgentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/api/v1/agents/")
+@RestController
+public class AgentRestController {
+
+    @Autowired
+    private IAgentService service;
+
+    @GetMapping("")
+    public List<Agent> showAll() {
+        return service.getAll();
+    }
+
+    @GetMapping("{id}")
+    public Agent showOne(@PathVariable String id){
+        return service.get(id);
+    }
+
+    @DeleteMapping("{id}")
+    public void del(@PathVariable String id){
+        service.delete(id);
+    }
+
+    @PostMapping()
+    public Agent insertOne(@RequestBody Agent agent){
+        return service.create(agent);
+    }
+    @PutMapping()
+    public Agent updateOne(@RequestBody Agent agent){
+        return service.update(agent);
+    }
+}
